4232b456d4a36d13ae068bf810517568b442872d
Arnaud15 <arnaud.autef@gmail.com>
2019-02-27 17:59:26 -0800

    v0 semantic_parser done

--- src/tokens_vocab.py
+++ src/tokens_vocab.py
@@ -2,9 +2,13 @@ from pytorch_pretrained_bert.tokenization import BertTokenizer
 from utils import pad_ids_sequences
 import torch
 
+#TODO verif que le dataset ne contienne pas de mots inconnus, auquel cas les rajouter
 class Vocab(object):
     def __init__(self, BERT_model):
         self.tokenizer = BertTokenizer.from_pretrained(BERT_model, do_lower_case=True)
+        # CUSTOM START AND END TOKENS ADDED
+        self.tokenizer.ids_to_tokens[1] = '[START]'
+        self.tokenizer.ids_to_tokens[1] = '[END]'
 
     def to_input_tensor(self, input_strings_sequences, device):
         """
@@ -19,3 +23,8 @@ class Vocab(object):
 
     def to_input_tokens(self, input_strings_sequences):
         return [self.tokenizer.tokenize(sequence) for sequence in input_strings_sequences]
+
+    def tokens_to_tensor(self, input_tokens_sequences, device):
+        token_ids_sequences = [self.tokenizer.convert_tokens_to_ids(tokens) for tokens in input_tokens_sequences]
+        padded_ids_sequences = pad_ids_sequences(token_ids_sequences)
+        return torch.tensor(padded_ids_sequences, dtype=torch.long, device=device)
